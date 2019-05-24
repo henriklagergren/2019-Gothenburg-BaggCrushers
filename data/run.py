7a9658e2acb46bfd0f3ca0f2c1bd94c8b235694e
AronSeamountain <aronsj@student.chalmers.se>
2019-05-24 21:52:10 +0200

    Added script to read aid data

--- data/run.py
+++ data/run.py
@@ -1,4 +1,11 @@
 import numpy as np
 import pandas as pd
 
-print(hello_world)
+print("hello_world")
+
+test = 7
+
+aidPerCountry = pd.read_excel("./aid/aid_from_sida.xlsx")
+
+aidPerCountry = aidPerCountry[['recipient-country','transaction-value-sek']]
+print(aidPerCountry.groupby('recipient-country') ['transaction-value-sek'].sum())
