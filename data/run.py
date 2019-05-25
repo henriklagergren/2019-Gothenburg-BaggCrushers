51c274fcf49e047a87d94b4e0728f47e78834f2b
ostcarl <ostcarl@student.chalmers.se>
2019-05-25 14:58:20 +0200

    json created

--- data/run.py
+++ data/run.py
@@ -1,11 +1,28 @@
 import numpy as np
 import pandas as pd
-
+import json
 print("hello_world")
 
 test = 7
 
 aidPerCountry = pd.read_excel("./aid/aid_from_sida.xlsx")
 
-aidPerCountry = aidPerCountry[['recipient-country','transaction-value-sek']]
-print(aidPerCountry.groupby('recipient-country') ['transaction-value-sek'].sum())
+aidPerCountry = aidPerCountry[['recipient-country','transaction-value-sek', 'recipient-country-code']]
+aidPerCountry.columns = ["country","aid_received_sek","country_code"]
+aidPerCountry = aidPerCountry.groupby(['country',"country_code"])['aid_received_sek'].sum()
+aidPerCountry = aidPerCountry.to_frame()
+corruptionPerCountry = pd.read_excel("./corruption/2018_CPI_FullDataSet.xlsx")
+corruptionPerCountry = corruptionPerCountry[['Country','CPI Score 2018']]
+corruptionPerCountry.columns = ["country","CPI"]
+#print(corruptionPerCountry)
+print(aidPerCountry)
+
+result = aidPerCountry.join(corruptionPerCountry, on="country", how='outer')
+
+#result = aidPerCountry.join(corruptionPerCountry, on="Country")
+#result = result.to_json(orient='table')
+Export = result.to_json (r'.\output.json', orient="table")
+#print(result)
+
+#with open('result.json','w') as outfile:
+    #json.dump(result, outfile)
