a9b80aa9bbcc7ea6011f4ed6149a8dabb3e3e964
henriklagergren <henriklagergren@hotmail.com>
2019-05-25 17:29:13 +0200

    Added searchbar and filters

--- flutter/lib/MainView.dart
+++ flutter/lib/MainView.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert';
 import 'CountryInformation.dart';
 import 'DetailedView.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class MainView extends StatefulWidget {
   @override
@@ -21,10 +22,24 @@ enum FILTERVALUES{
 class _MainViewState extends State<MainView> {
 
  
-
-
   FILTERVALUES dropdownValue = FILTERVALUES.CORRUPTION;
-  bool descending = false;  
+  bool descending = false;
+  String userSearch = "";
+  ScrollController _scrollController;
+  TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   List<CountryInformation> sortBy(FILTERVALUES property, List<CountryInformation> list){
     if(property == FILTERVALUES.CORRUPTION){
@@ -32,67 +47,113 @@ class _MainViewState extends State<MainView> {
     }else if(property == FILTERVALUES.AID){
       list.sort((a, b) => a.aidMoney.compareTo(b.aidMoney));
     }
+    
+    if(userSearch != ""){
+    List<CountryInformation> filteredList = new List();
+    for (var countryInfo in list) {
+      if(countryInfo.countryName.contains(userSearch)){
+        filteredList.add(countryInfo);
+      }
+    }
+    return filteredList;
+    }else{
     return list;
+    }
   }
   
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return WillPopScope(
+      onWillPop: (){
+        setState(() {
+          userSearch = "";
+          _textEditingController.clear();
+        });
+      },
+          child: Scaffold(
 
-      appBar: PreferredSize(
-        preferredSize: Size(60,60),
-        child: Row(
-          children: <Widget>[
-            SizedBox(height: 30),
-            DropdownButton<FILTERVALUES>(
-                value: dropdownValue,
-                onChanged: (FILTERVALUES newValue) {
-                  setState(() {
-                    dropdownValue = newValue;
-                  });
-                },
-                items: <FILTERVALUES>[FILTERVALUES.CORRUPTION, FILTERVALUES.AID]
-                    .map<DropdownMenuItem<FILTERVALUES>>((FILTERVALUES value) {
-                  return DropdownMenuItem<FILTERVALUES>(
-                    value: value,
-                    child: Text(value.toString().split('.').last),
-                  );
-                }).toList(),
-              )
-        ],),
+        appBar: PreferredSize(
+          preferredSize: Size(double.maxFinite,130),
+          child: Container(
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 40),
+                _SearchBar((search) {
+                        setState(() {
+                          userSearch = search;
+                        });
+                        _scrollController.animateTo(
+                            _scrollController.position.minScrollExtent,
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.ease);
+                      }, "Sök land",_textEditingController),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    DropdownButton<FILTERVALUES>(
+                    value: dropdownValue,
+                    onChanged: (FILTERVALUES newValue) {
+                      setState(() {
+                        dropdownValue = newValue;
+                      });
+                    },
+                    items: <FILTERVALUES>[FILTERVALUES.CORRUPTION, FILTERVALUES.AID]
+                        .map<DropdownMenuItem<FILTERVALUES>>((FILTERVALUES value) {
+                      return DropdownMenuItem<FILTERVALUES>(
+                        value: value,
+                        child: Text(value.toString().split('.').last),
+                      );
+                    }).toList(),
+                  ),
+                   IconButton(
+                    onPressed: () => setState((){
+                      descending = !descending;
+                    }),
+                    icon: Icon(descending? MaterialCommunityIcons.getIconData("sort-descending"):MaterialCommunityIcons.getIconData("sort-ascending"),size: 30,),
+                  ),
+                  ],
+                ),
+               
+            ],
+            ),
+          ),
 
-      ),
-      body: FutureBuilder(
-            future: loadJson(),
-            builder: (BuildContext context, AsyncSnapshot snapshot){
-              if(snapshot.hasData){
-                var data = json.decode(snapshot.data);
+        ),
+        body: FutureBuilder(
+              future: loadJson(),
+              builder: (BuildContext context, AsyncSnapshot snapshot){
+                if(snapshot.hasData){
+                  var data = json.decode(snapshot.data);
                 var rest = data["data"] as List;
                 List<CountryInformation> list = rest.map<CountryInformation>((json) => CountryInformation.fromJson(json)).toList();
                 list.removeWhere((value) => value == null); // This should probably not be done this way since it takes O(n)
                 list = sortBy(dropdownValue, list);
-                
-                return CardTiles(list);
-              }else{
-                return CircularPercentIndicator(radius: 10,);
-              }
-            },
-          ),
-        
-    
+                  
+                  return CardTiles(list,_scrollController);
+                }else{
+                  return CircularPercentIndicator(radius: 10,);
+                }
+              },
+            ),
+          
+      
+      ),
     );
   }
 }
 
 class CardTiles extends StatelessWidget {
   final List<CountryInformation> _countries;
+  final _scrollController;
 
-  CardTiles(this._countries);
+  CardTiles(this._countries,this._scrollController);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _countries.length,
       itemBuilder: (BuildContext context, int index) {
         return CardTile(_countries.elementAt(index));
@@ -182,6 +243,35 @@ class CardTile extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _SearchBar extends StatelessWidget {
+  final Function _onChange;
+  final String _hintText;
+  final TextEditingController _controller;
+
+  _SearchBar(this._onChange, this._hintText, this._controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: StadiumBorder(),
+      elevation: 5,
+      child: new TextField(
+        controller: _controller,
+        decoration: new InputDecoration(
+            hintText: _hintText,
+            border: InputBorder.none,
+            prefixIcon: new Icon(
+              Icons.search,
+              size: 19,
+            )),
+        onChanged: (String search) {
+          _onChange(search);
+        },
       ),
     );
   }
